4429275c8f7b5861a0996c8aa3a4968fa3ef36b9
KiRinXC <guowenquan24@mails.ucas.ac.cn>
2024-12-20 23:06:45 +0800

    文本生成

--- pipelines/TextGeneration.py
+++ pipelines/TextGeneration.py
@@ -9,3 +9,21 @@ results = generator(
     max_length=20
 )
 print(results)
+
+generator = pipeline("text-generation", model="distilgpt2")
+results = generator(
+    "In this course, we will teach you how to",
+    max_length=20,
+    num_return_sequences=1,
+)
+print(results)
+
+
+generator = pipeline("text-generation", model="uer/gpt2-chinese-poem")
+results = generator(
+    "[CLS] 梅 山 如 积 翠 ，",
+    max_length=40,
+    do_sample=True,
+)
+print(results)
+
